b56e44e32c32eb2c4935563d6a92f2ecab83bcfe
Piirainen Vanamo K <vseppane@lx7-fuxi359.ad.helsinki.fi>
2018-09-03 15:54:03 +0300

    Kirjautumisen navigointi

--- App.js
+++ App.js
@@ -2,16 +2,35 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import store from './app/store'
+import { createSwitchNavigator, createStackNavigator } from 'react-navigation'
+import HomeScreen from './app/components/HomeScreen'
+import SignInScreen from './app/components/SignInScreen'
+import AuthLoadingScreen from './app/components/AuthLoadingScreen'
+
+const AppStack = createStackNavigator({
+  Home: HomeScreen
+})
+
+const AuthStack = createStackNavigator({
+  SignIn: SignInScreen
+})
+
+const RootStack = createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack
+  },
+  {
+    initialRouteName: 'AuthLoading'
+  }
+)
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-        </View>
+        <RootStack/>
       </Provider>
     );
   }
