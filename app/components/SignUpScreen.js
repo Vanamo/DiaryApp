e171e0723388ba296c268dd4851894f91d80aa93
Piirainen Vanamo K <vseppane@lx7-fuxi359.ad.helsinki.fi>
2018-09-06 14:10:16 +0300

    signUp-sivun tyylin parantelua

--- app/components/SignUpScreen.js
+++ app/components/SignUpScreen.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import { register } from '../reducers/authReducer'
 import { newErrorNotification } from '../reducers/notificationReducer'
 import Notification from './Notification'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { Button } from 'react-native-elements'
+import { Button, Tile } from 'react-native-elements'
 
 class SignUpScreen extends React.Component {
 
@@ -37,12 +37,19 @@ class SignUpScreen extends React.Component {
     return (
       <KeyboardAwareScrollView
         keyboardShouldPersistTaps='always'
+        enableOnAndroid={true}
       >
-        <View style={styles.container}>
-          <Button
-            title='testi'
+        <View style={styles.formContainer}>
+          <Tile
+            imageSrc={require('../../images/writing-1209121_1920.jpg')}
+            containerStyle={{ height: 200 }}
           />
-          <Text>Luo käyttäjätunnus</Text>
+          <Button
+            title='Onko sinulla jo käyttäjätunnus? Kirjaudu sisään tästä'
+            onPress={() => this.props.navigation.navigate('Login')}
+            borderRadius={4}
+          />
+          <Text style={styles.title}>Luo käyttäjätunnus</Text>
           <Notification />
           <TextInput
             placeholder='Käyttäjätunnus'
@@ -50,6 +57,7 @@ class SignUpScreen extends React.Component {
             style={styles.textInput}
             onChangeText={username => this.setState({ username })}
             value={this.state.username}
+            underlineColorAndroid='transparent'
           />
           <TextInput
             placeholder='Sähköpostiosoite'
@@ -57,6 +65,7 @@ class SignUpScreen extends React.Component {
             style={styles.textInput}
             onChangeText={email => this.setState({ email })}
             value={this.state.email}
+            underlineColorAndroid='transparent'
           />
           <TextInput
             secureTextEntry
@@ -65,22 +74,13 @@ class SignUpScreen extends React.Component {
             style={styles.textInput}
             onChangeText={password => this.setState({ password })}
             value={this.state.password}
-          />
-          <TextInput
-            secureTextEntry
-            placeholder='Salasana uudestaan'
-            autoCapitalize='none'
-            style={styles.textInput}
-            onChangeText={cPassword => this.setState({ cPassword })}
-            value={this.state.cPassword}
+            underlineColorAndroid='transparent'
           />
           <Button
             title='Luo käyttäjätunnus'
             onPress={this.handleSignUp}
-          />
-          <Button
-            title='Onko sinulla jo käyttäjätunnus? Kirjaudu sisään tästä'
-            onPress={() => this.props.navigation.navigate('Login')}
+            borderRadius={4}
+            buttonStyle={{ marginTop: 15 }}
           />
         </View>
       </KeyboardAwareScrollView>
@@ -89,17 +89,22 @@ class SignUpScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  formContainer: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'flex-start'
+  },
+  title: {
+    marginLeft: 13,
+    marginTop: 20
   },
   textInput: {
     height: 40,
     width: '90%',
     borderColor: 'gray',
     borderWidth: 1,
-    marginTop: 8
+    marginTop: 8,
+    marginLeft: 13
   }
 })
 
