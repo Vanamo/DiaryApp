cc39e8aa190dc51b00953fd51955a450a3b37399
Vanamo Piirainen <vanamo.piirainen@helsinki.fi>
2018-11-19 18:40:06 +0200

    constants viety gittiin

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = 'AIzaSyBocwQcVMwJSd1yCbl3Rh4hsX9K8SoYZ1c'
+export const FIREBASE_AUTH_DOMAIN = 'diary-app-14a93.firebaseapp.com'
+export const FIREBASE_DATABASE_URL = 'https://diary-app-14a93.firebaseio.com'
+export const FIREBASE_PROJECT_ID = 'diary-app-14a93'
+export const FIREBASE_STORAGE_BUCKET = 'diary-app-14a93.appspot.com'
+export const FIREBASE_MESSAGING_SENDER_ID = '490683827527'
+
+export const MAX_YEARS = 10
